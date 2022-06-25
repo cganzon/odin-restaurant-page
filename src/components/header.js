a23212c3e10fc579219d1ceb43e36eaaab13a5ba
cganzon <crganzon@gmail.com>
2022-06-24 19:49:59 -0700

    move javascript header creation to a separate file

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+const header = () => {
+  const header = document.createElement("header");
+  const restaurantName = document.createElement("h1");
+  const nav = document.createElement("nav");
+  const navLinkOne = document.createElement("a");
+  const navLinkTwo = document.createElement("a");
+
+  restaurantName.textContent = "Caesarino's";
+  navLinkOne.textContent = "Home";
+  navLinkOne.setAttribute("href", "");
+  navLinkOne.classList.add("home");
+  navLinkTwo.textContent = "Menu";
+  navLinkTwo.setAttribute("href", "");
+  navLinkTwo.classList.add("menu");
+
+  nav.append(navLinkOne, navLinkTwo);
+  header.append(restaurantName, nav);
+
+  return header;
+};
+
+export default header;
