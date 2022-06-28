885d315b637d0f7e46f3a0eb6be72d2597c280bd
cganzon <crganzon@gmail.com>
2022-06-27 19:27:26 -0700

    change header styles and add wrapper div to the hero text

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,7 @@ const home = () => {
 
   const hero = document.createElement("section");
   const heroImage = document.createElement("img");
+  const heroText = document.createElement("div");
   const heroHeading = document.createElement("h2");
   const heroLead = document.createElement("p");
 
@@ -15,12 +16,16 @@ const home = () => {
     heroImageFile
   );
   heroImage.setAttribute("alt", "");
+
+  heroText.classList.add("hero-text");
   heroHeading.textContent =
     "Enjoy a variety of freshly made pasta to satisfy your craving";
   heroLead.textContent =
     "Caesarino's is the best place for authentic italian pasta. Our staff is top notch and the atmosphere makes you feel right at home. If you're ever in the mood for pasta, Caesarino's is the place to be!";
 
-  hero.append(heroImage, heroHeading, heroLead);
+  heroText.append(heroHeading, heroLead)
+
+  hero.append(heroImage, heroText);
   homeContent.append(hero);
 
   return homeContent;
